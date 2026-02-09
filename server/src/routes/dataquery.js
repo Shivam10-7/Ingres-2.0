baab03ee184de21bac4fbf52d6bba0487d5eb192
Shivam10-7 <shivampilley@gmail.com>
2026-02-09 20:07:57 +0530

    Made pseudo code for the data query pipeline

---
+++ server/src/routes/dataquery.js
@@ -0,0 +1,7 @@
+// this is dataquery routes
+function dataqueryHandler(Query){
+    // the sql generator
+    // the data extractor
+    // the model for genarating response
+    // return the response example "the weter level of bathinda is xxx meters"
+}
