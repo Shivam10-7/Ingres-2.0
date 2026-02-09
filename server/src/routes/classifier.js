02c2627512b361c811579bce9ca81541e89a8503
Shivam10-7 <shivampilley@gmail.com>
2026-02-09 21:32:17 +0530

    Added comments to the classifier.js

--- server/src/routes/classifier.js
+++ server/src/routes/classifier.js
@@ -1,20 +1,20 @@
 function selectPipeline(isDetailedResponseNeeded, isVisualizationNeeded) {
   if (!isDetailedResponseNeeded && !isVisualizationNeeded) {
     console.log("Data query pipeline selected");
-    return "DATA_QUERY_PIPELINE";
+    return "DATA_QUERY_PIPELINE";//here the function would be callled that would call the data query pipeline and then return the response to the user
   }
 
   if (isDetailedResponseNeeded && !isVisualizationNeeded) {
     console.log("Detailed data query pipeline selected");
-    return "DETAILED_DATA_QUERY_PIPELINE";
+    return "DETAILED_DATA_QUERY_PIPELINE";//here the function would be callled that would call the detailed data query pipeline and then return the response to the user
   }
 
   if (!isDetailedResponseNeeded && isVisualizationNeeded) {
     console.log("Visualization pipeline selected");
-    return "VISUALIZATION_PIPELINE";
+    return "VISUALIZATION_PIPELINE";//here the function would be callled that would call the visualization pipeline and then return the response to the user
   }
     console.log("Analytics pipeline selected");
-  return "ANALYTICS_PIPELINE";
+  return "ANALYTICS_PIPELINE";//here the function would be callled that would call the analytics pipeline and then return the response to the user
 }
 
 module.exports = selectPipeline;
