6ddc32adb937d7301ed309b21c8a577819de284a
Shivam10-7 <shivampilley@gmail.com>
2026-02-20 00:12:22 +0530

    made the payload maker for the echarts but didnt implement it

---
+++ server/src/routes/ChartData/BarData.js
@@ -0,0 +1,19 @@
+function buildBarChart(data, title) {
+  return {
+    title: { text: title },
+    tooltip: {},
+    xAxis: {
+      type: "category",
+      data: data.rows.map(r => r[0])
+    },
+    yAxis: { type: "value" },
+    series: [
+      {
+        type: "bar",
+        data: data.rows.map(r => r[1])
+      }
+    ]
+  };
+}
+
+module.exports = buildBarChart;
